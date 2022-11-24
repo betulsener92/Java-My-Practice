c8ed014c05e06a04aa1f67a8e17aaa62ce8ffa67
Betul <betulsener92@gmail.com>
2022-11-24 20:11:18 +0300

    math class kullanildi

---
+++ src/main/java/practice4/RastgeleAtama.java
@@ -0,0 +1,43 @@
+package practice4;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+public class RastgeleAtama {
+    //Task:
+    // Matematik dersinden 6 farkli konu anlatilacaktir.Ogretmen sinifi üc gruba ayırmıştır ve her bir gruba, icinde konu basliklari yazili
+    // olan bir torbayı rastgele atayacak kodu yaziniz
+
+    public static void main(String[] args) {
+
+        HashMap<String, String> eslesme = new HashMap<>();
+        Set<String> grupSet = eslesme.keySet();
+        List<Integer> list = new ArrayList<>();
+
+        String gruplar[] = {"1. Grup", "2. Grup", "3. Grup"};
+        String topbalar[] = {" 1. Torba", " 2. Torba", " 3. Torba", " 4. Torba", " 5. Torba", " 6. Torba"};
+
+
+        boolean secim = true;
+        while (secim) {
+            int grup = (int) (Math.random() * 3);
+            int torba = (int) (Math.random() * 6);
+            if (list.size()<=3) {
+                list.add(grup);
+                if (!grupSet.contains(gruplar[grup])) {
+                    eslesme.put(gruplar[grup], topbalar[torba]);
+                }
+            }else{
+                secim = false;
+            }
+
+
+        }
+
+        System.out.println(eslesme);
+
+    }
+    }
